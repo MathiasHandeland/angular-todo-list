3aba439b3ba8a51624c83efec3fb44baa1625084
Mathias Handeland <127216029+MathiasHandeland@users.noreply.github.com>
2025-09-09 12:48:32 +0200

    extention done

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,26 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 import { Todo } from '../models/todo';
+import { Observable, BehaviorSubject, switchMap, map } from 'rxjs';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css'],
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+  todos$!: Observable<Todo[]>; 
+  private showCompleted = false; 
+
   constructor(private readonly todoService: TodoService) {}
 
-  todos = this.todoService.getTodos();
+  ngOnInit() {
+    this.loadTodos();
+  }
+
+  toggleView() {
+    this.showCompleted = !this.showCompleted;
+    this.loadTodos();
+  }
 
   updateTodo(todo: Todo) {
     this.todoService.updateTodo(todo).subscribe(() => {
-      this.todos = this.todoService.getTodos();
+      this.loadTodos();
     });
   }
 
   newTodo(title: string) {
     this.todoService.addTodo(title).subscribe(() => {
-      this.todos = this.todoService.getTodos();
+      this.loadTodos();
     });
+  }
+
+  private loadTodos() {
+    this.todos$ = this.todoService.getTodos().pipe(
+      map(todos =>
+        todos.filter(todo => todo.completed === this.showCompleted)
+      )
+    );
   }
 }
